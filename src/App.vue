892e5c535767645ad9fc7a3690e9a35c4dbc7772
panzhiyue <30484534+panzhiyue@users.noreply.github.com>
2022-05-16 14:16:40 +0800

    vue2ol示例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue2ol-map>
+      <vue2ol-view :options="viewOptions"></vue2ol-view>
+      <vue2ol-layer-tile>
+        <vue2ol-source-osm></vue2ol-source-osm>
+      </vue2ol-layer-tile>
+      <vue2ol-layer-vector>
+        <vue2ol-source-vector :features="features"> </vue2ol-source-vector>
+      </vue2ol-layer-vector>
+    </vue2ol-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as proj from "ol/proj";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      viewOptions: {
+        center: [0, 0],
+        projection: proj.get("EPSG:4326"),
+        zoomFactor: 2,
+        maxZoom: 18,
+        zoom: 5,
+      },
+      features: [],
+    };
+  },
+  mounted() {
+    let i = 0;
+    setInterval(() => {
+      i++;
+      let coordinate = [0.1 * i, 0.1 * i];
+      let feature = new Feature({
+        geometry: new Point(coordinate),
+      });
+      this.features = [feature];
+    }, 1000);
+  },
+};
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vue2ol from "@gis-js/vue2ol"
+
+Vue.use(Vue2ol);
 
 Vue.config.productionTip = false
 
